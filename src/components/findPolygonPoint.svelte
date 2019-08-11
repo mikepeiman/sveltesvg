3e3f5c7117dd6514ff0a7bbb994da1d2a55e6981
Mike Peiman <mikepeiman@gmail.com>
2019-08-11 13:56:27 -0700

    Singificant work refactoring to imported components and functions, implemented Svelte store to share polygonPointsArray. Now getting an unexpected cool effect

---
+++ src/components/findPolygonPoint.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+  import { coords } from "./stores.js";
+  export function findPolygonPoint(x, y, svgElement, points, distance, iteration, polygonPointsArray) {
+      let svg = svgElement;
+      let polygon = svg.childNodes[0];
+      let angle = (360 / points) * iteration;
+    ``
+      console.log(
+        `angle: ${angle} x ${x}, y ${y}, svgElement ${svgElement}, points ${points}, distance ${distance}, iteration ${iteration}`
+      );
+      let result = {};
+      let newX = Math.round(Math.cos((angle * Math.PI) / 180) * distance + x);
+      let newY = Math.round(Math.sin((angle * Math.PI) / 180) * distance + y);
+      angle += angle;
+      if (iteration <= 0) {
+        console.log(`base condition of findPolygonPoint: array ${polygonPointsArray}`);
+        coords.set(polygonPointsArray)
+        return polygonPointsArray;
+      }
+      result.x = x;
+      result.y = y;
+      // polygonPointsArray = [...polygonPointsArray, [x, y]];
+      console.log(`x ${x} and y ${y}: newX ${newX}, newY ${newY}`);
+      console.log(`polygonPointsArray = ${polygonPointsArray}`);
+      console.log(polygonPointsArray);
+      
+      let point = svg.createSVGPoint();
+      point.x = x;
+      point.y = y;
+      // polygonPointsArray.update(arr => [...arr, point]);
+      polygonPointsArray = [...polygonPointsArray, point]
+      findPolygonPoint(newX, newY, svg, points, distance, iteration - 1, polygonPointsArray);
+      // return result;
+    }
+</script>
--- src/components/svg-element.svelte
+++ src/components/svg-element.svelte
@@ -1,13 +1,21 @@
 <script>
   // import { css } from 'emotion'
   import { onMount } from "svelte";
-  export let headline = "Default text";
-  export let bg = "rgba(60,120,180,1)";
+  import { coords } from "./stores.js";
+  import { Polygon } from "./polygon.js";
+  import { findPolygonPoint } from "./findPolygonPoint.svelte";
+  import { writable } from "svelte/store";
+
   $: x = 0;
   $: y = 0;
   let points = 5;
-  $: polygonPointsArray = [];
   let distance = 60;
+  // let polygonPointsArray = []
+
+  let polygonPointsArray;
+  const unsubscribe = coords.subscribe(value => {
+    polygonPointsArray = value;
+  });
 
   onMount(() => {
     let svg = document.getElementById("pentagon");
@@ -19,87 +27,10 @@
     svg.appendChild(polygon);
   });
 
-  function Polygon() {
-    // from https://gist.github.com/roboncode/4bf663b53d2dcf2f05efea22e8a20ea8
-
-    let pointsList = [];
-    let svg = document.getElementById("pentagon");
-    this.node = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "polygon"
-    );
-
-    function build(arg) {
-      let res = [];
-      for (let i = 0, l = arg.length; i < l; i++) {
-        console.log(`inside of Polygon.build(): res ${res}`);
-        res.push(arg[i].join(","));
-      }
-      return res.join(" ");
-    }
-
-    this.attribute = function(key, val) {
-      if (val === undefined) return this.node.getAttribute(key);
-      this.node.setAttribute(key, val);
-    };
-
-    this.getPoint = function(i) {
-      return pointsList[i];
-    };
-
-    this.setPoint = function(i, x, y) {
-      pointsList[i] = [x, y];
-      this.attribute("points", build(pointsList));
-    };
-
-    this.points = function(points, l) {
-      for (let i = 0; i < l; i += 1) {
-        console.log(
-          `i ${i} l ${l} this.points arguments[i] ${points[i]}, points[i+1] ${
-            points[i + 1]
-          } and pointsList: ${pointsList}`
-        );
-        if (points[i + 1] !== undefined) {
-          pointsList.push([points[i], points[i + 1]]);
-        }
-      }
-      this.attribute("points", build(pointsList));
-    };
-
-    this.points.apply(this, arguments);
-  }
-
-  function findPolygonPoint(x, y, svgElement, points, distance, iteration) {
-    let svg = svgElement;
-    let polygon = svg.childNodes[0];
-    let angle = (360 / points) * iteration;
-
-    console.log(
-      `angle: ${angle} x ${x}, y ${y}, svgElement ${svgElement}, points ${points}, distance ${distance}, iteration ${iteration}`
-    );
-    let result = {};
-    let newX = Math.round(Math.cos((angle * Math.PI) / 180) * distance + x);
-    let newY = Math.round(Math.sin((angle * Math.PI) / 180) * distance + y);
-    angle += angle;
-    if (iteration <= 0) {
-      return polygonPointsArray;
-    }
-    result.x = x;
-    result.y = y;
-    polygonPointsArray = [...polygonPointsArray, [x, y]];
-    console.log(`x ${x} and y ${y}: newX ${newX}, newY ${newY}`);
-    console.log(`polygonPointsArray = ${polygonPointsArray}`);
-    let point = svg.createSVGPoint();
-    point.x = x;
-    point.y = y;
-    findPolygonPoint(newX, newY, svg, points, distance, iteration - 1);
-    // return result;
-  }
-
-  export function getOrigin(e) {
+  export function createPolygonAtClick(e) {
     let svg = document.getElementById("pentagon");
     let polygon = svg.childNodes[0];
-    polygonPointsArray = [];
+    // polygonPointsArray = [];
     let iteration = points;
 
     // code to generate svg centered on click
@@ -108,17 +39,37 @@
     y = e.y - rect.top + distance;
     // end code to generate svg centered on click
 
-    console.log(`getOrigin x ${x}, y ${y}, and e:`);
+    console.log(`createPolygonAtClick x ${x}, y ${y}, and e:`);
     console.log(e);
 
-    console.log(`inside getOrigin for loop: findPolygon returns: `);
-    findPolygonPoint(x, y, svg, points, distance, iteration);
+    // console.log(`inside createPolygonAtClick for loop: findPolygonPoint returns: `);
+    findPolygonPoint(
+      x,
+      y,
+      svg,
+      points,
+      distance,
+      iteration,
+      polygonPointsArray
+    );
 
     console.log("polygonPointsArray");
     console.log(polygonPointsArray);
+    polygonPointsArray.forEach(point => {
+      console.log(`point: ${point}`);
+      console.log(point);
+      console.log(`${point.x},${point.y}`);
+      
+      
+      
+    })
+    let mapped = polygonPointsArray.map(point => [`${point.x},${point.y}`])
+    console.log(`mapped: ${mapped}`);
+    
 
     let penta = new Polygon();
-    penta.points(polygonPointsArray, polygonPointsArray.length);
+    // penta.points(polygonPointsArray, polygonPointsArray.length);
+    penta.points(mapped, mapped.length)
 
     let elX = svg.parentNode.offsetWidth;
     let elY = svg.parentNode.offsetHeight;
@@ -128,21 +79,20 @@
     let hueX = x * hueFactorX;
     let hueY = y * hueFactorY;
     let alphaY = (y * aFactorY).toFixed(2);
-    console.log(`alpha factor: ${aFactorY}, alphaY ${alphaY}`);
-    
+    // console.log(`alpha factor: ${aFactorY}, alphaY ${alphaY}`);
     penta.attribute("fill", `hsla(${hueX},100%,50%,${alphaY})`);
-    console.log(
-      `!!!!!!!!!!!!!!!!!!!!!!!!!!!!! svg parent width ${elX} ${hueFactorX}, ${elY} ${hueFactorY}`
-    );
-    console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!! hueX ${hueX} hueY ${hueY}`);
+    // console.log(
+    //   `!!!!!!!!!!!!!!!!!!!!!!!!!!!!! svg parent width ${elX} ${hueFactorX}, ${elY} ${hueFactorY}`
+    // );
+    // console.log(`!!!!!!!!!!!!!!!!!!!!!!!!!!!!! hueX ${hueX} hueY ${hueY}`);
 
-    console.log(polygonPointsArray.length);
-    console.log("penta");
-    console.log(penta);
+    // console.log(polygonPointsArray.length);
+    // console.log("penta");
+    // console.log(penta);
     svg.appendChild(penta.node);
 
-    console.log("penta.points");
-    console.log(penta.points);
+    // console.log("penta.points");
+    // console.log(penta.points);
   }
 </script>
 
@@ -182,7 +132,7 @@
   }
 </style>
 
-<div class="svg-card" on:click={getOrigin}>
+<div class="svg-card" on:click={createPolygonAtClick}>
   <svg id="pentagon" />
   <!-- <svg height="210" width="500">
     <polygon
