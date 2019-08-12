821b44d80f4d04a8d594d7347b763e1b50868525
Mike Peiman <mikepeiman@gmail.com>
2019-08-12 13:06:32 -0700

    Made much progress in layering polygon gradients, but not yet sure how to reliably center identical polygons when transforming via rotation. transform-origin is close, but not perfect

--- src/components/gradient-svg.svelte
+++ src/components/gradient-svg.svelte
@@ -5,9 +5,6 @@
   import { Polygon } from "./polygon.js";
   import { findPolygonPoint } from "./findPolygonPoint.svelte";
   import { writable } from "svelte/store";
-
-
-
 </script>
 
 <style>
@@ -30,7 +27,7 @@
   }
 
   svg {
-    stroke: black;
+    /* stroke: black; */
     position: absolute;
     width: 100%;
     height: 100%;
@@ -39,26 +36,170 @@
     z-index: -1;
   }
 
-  #pentagon {
-    fill: rgba(75, 125, 175, 0.75);
-    stroke: rgba(75, 125, 175, 1);
-    stroke-width: 5;
+  polygon {
+        position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    stroke-width: 0;
+  }
 
+  #pentagon-1 {
+    /* fill: rgba(75, 125, 175, 0.75); */
+    /* stroke: rgba(75, 125, 175, 1); */
+    stroke-width: 0;
   }
 </style>
 
 <div class="svg-card">
 
- <svg height="500" width="500" id="pentagon">
-   <defs>
-      <radialGradient id="Gradient1" gradientTransform="scale(0.5,2)" cx="50%" cy="55%" r="50%" fx="10%" fy="50%">
-        <stop class="stop1" offset="0%" stop-color="gold" stop-opacity="1"/>
-        <stop class="stop2" offset="50%" stop-color="green" stop-opacity=".5"/>
-        <stop class="stop3" offset="100%" stop-color="blue" stop-opacity=".25" />
-      </radialGradient>
+  <svg height="500" width="500" id="pentagon">
+    <defs>
+      <!-- <radialGradient
+        id="gradientRad1"
+        gradientTransform="rotate(72)"
+        cx="50%"
+        cy="50%"
+        r="50%"
+        fx="50%"
+        fy="50%">
+        <stop class="stop1" offset="0%" stop-color="red" stop-opacity="1" />
+        <stop class="stop2" offset="50%" stop-color="white" stop-opacity="0" />
+        <stop
+          class="stop3"
+          offset="100%"
+          stop-color="blue"
+          stop-opacity="1" />
+      </radialGradient> -->
+      <linearGradient
+        id="fire"
+        x1="0%"
+        x2="100%"
+        y1="0%"
+        y2="0%">
+        <stop class="stop1" offset="0%" stop-color="red" stop-opacity="1" />
+        <stop class="stop2" offset="100%" stop-color="white" stop-opacity="0" />
+
+      </linearGradient>
+      <linearGradient
+        id="earth"
+        x1="0%"
+        x2="100%"
+        y1="0%"
+        y2="0%">
+        <stop class="stop1" offset="0%" stop-color="brown" stop-opacity="1" />
+        <stop class="stop2" offset="100%" stop-color="white" stop-opacity="0" />
+
+      </linearGradient>
+      <linearGradient
+        id="metal"
+        x1="0%"
+        x2="100%"
+        y1="0%"
+        y2="0%">
+        <stop class="stop1" offset="0%" stop-color="grey" stop-opacity="1" />
+        <stop class="stop2" offset="100%" stop-color="white" stop-opacity="1" />
+
+      </linearGradient>
+      <linearGradient
+        id="water"
+        x1="0%"
+        x2="100%"
+        y1="0%"
+        y2="0%">
+        <stop class="stop1" offset="0%" stop-color="blue" stop-opacity="1" />
+        <stop class="stop2" offset="100%" stop-color="white" stop-opacity="0" />
+
+      </linearGradient>
+      <linearGradient
+        id="wood"
+        x1="0%"
+        x2="100%"
+        y1="0%"
+        y2="0%">
+        <stop class="stop1" offset="0%" stop-color="green" stop-opacity="1" />
+        <stop class="stop2" offset="100%" stop-color="white" stop-opacity="0" />
+
+      </linearGradient>
+
+      <polygon
+        id="polygon-fire"
+        transform="rotate(0)"
+        transform-origin="center"
+        points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+        417,34,304,116"
+        fill="url(#fire)" />
+      <polygon
+        id="polygon-earth"
+        transform="rotate(72)"
+        transform-origin="center"
+        points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+        417,34,304,116"
+        fill="url(#earth)" />
+      <polygon
+        id="polygon-metal"
+        transform="rotate(144)"
+        transform-origin="center"
+        points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+        417,34,304,116"
+        fill="url(#metal)" />
+      <polygon
+        id="polygon-water"
+        transform="rotate(216)"
+        transform-origin="center"
+        points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+        417,34,304,116"
+        fill="url(#water)" />
+      <polygon
+        id="polygon-wood"
+        transform="rotate(288)"
+        transform-origin="center"
+        points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+        417,34,304,116"
+        fill="url(#wood)" />
+      <filter id="blend-it" x="0" y="0" width="100%" height="100%">
+        <feImage xlink:href="#polygon-fire" result="fireR" x="0" y="0" width="100%" height="100%" />
+        <feImage xlink:href="#polygon-earth" result="earthR" x="0" y="0" width="100%" height="100%"/>
+        <feImage
+          xlink:href="#polygon-metal"
+          result="metalR"
+          x="0"
+          y="0"
+width="100%" height="100%"/>
+        <feImage
+          xlink:href="#polygon-water"
+          result="waterR"
+          x="0"
+          y="0"
+width="100%" height="100%" />
+        <feImage
+          xlink:href="#polygon-wood"
+          result="woodR"
+          x="0"
+          y="0"
+width="100%" height="100%"/>
+        <feBlend in="fireR" in2="earthR" mode="multiply" result="fire-earth" />
+        <feBlend in="waterR" in2="woodR" mode="multiply" result="water-wood" />
+        <feBlend
+          in="fire-earth"
+          in2="water-wood"
+          mode="multiply"
+          result="four-elements" />
+        <feBlend
+          in="four-elements"
+          in2="metalR"
+          mode="multiply"
+          result="five-elements" />
+        <feComposite in="five-elements" in2="SourceGraphic" operator="in" />
+      </filter>
+
     </defs>
     <polygon
-      points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34 417,34,304,116" 
-      fill="url('#Gradient1')" />
-  </svg> 
+      id="pentagon-1"
+      points="347.1999816894531,249.25,487,249 487,249,530,116 530,116,417,34
+      417,34,304,116"
+      filter="url(#blend-it)" />
+
+  </svg>
 </div>
